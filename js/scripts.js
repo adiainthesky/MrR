6bcbebf12ed872ef3b771d0597152de6f350af00
Adelaide Nalley <adiainthesky@gmail.com>
2020-10-07 20:15:09 -0700

    add all specs. Still issue with 0 and numbers above 9

--- js/scripts.js
+++ js/scripts.js
@@ -19,7 +19,32 @@ $(document).ready(function() {
 
    let reverseArray = newArray.reverse();
 
+
+      reverseArray.forEach(function(number) {
+    if (reverseArray.includes(1)) {
+      reverseArray[number] = "Beep!"; 
+    }
+    else if (reverseArray.includes(2)) {
+      reverseArray[number] = "Boop!" 
+    }
+
+  else if (reverseArray.includes(3)) {
+      reverseArray[number] = "Won't you be my neighbor?" 
+    };
+  });
+
+
+
+  // reverseArray.forEach(function(number, i) {
+  //   if (reverseArray === 0) { 
+  //     reverseArray[i] = "Beep!" 
+  //   };
+  // });
+
+
+
 console.log(reverseArray)
+
 
 $("#result").text(number);
 
